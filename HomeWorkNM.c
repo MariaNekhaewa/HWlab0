f5e774e5a8855b78e320b3847cfa32cafc3a08c9
Mara <Mara@DESKTOP-4BA3E9U>
2024-09-05 15:06:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkNM.c
@@ -0,0 +1,23 @@
+#include <locale.h>
+
+#include <stdio.h>
+
+void main()
+
+{
+
+	setlocale(LC_ALL, "RUS"); 
+
+	puts("           ___          ");
+	puts("           |  \\          ");
+	puts("           |   \\         "); 
+	puts("           |   |         ");
+	puts("           |  /          ");
+	puts("           | /           ");
+	puts("    _______|/____________");
+	puts("    |_  *  *  *  *  *  */ ");
+	puts("     |_________________/  ");
+
+
+
+}
